38ee6c836f09eb11caf4993642ffdabb9f7ac56d
skvinderr <80159890+skvinderr@users.noreply.github.com>
2021-10-15 19:45:49 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+import os
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+with open(os.path.join("helobomb", ".version"), "r", encoding="utf8") as fh:
+    version = fh.read().strip()
+
+with open("requirements.txt", "r", encoding="utf8") as fh:
+    requirements = fh.read().strip().splitlines()
+
+
+setup(
+    name='helobomb',
+    packages=find_packages(),
+    include_package_data=True,
+    version=version,
+    description='A free and open-source SMS,Call & Mail bombing application',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='TheSpeedX',
+    author_email='skvinderr@gmail.com',
+    url='https://github.com/skvinderr/hellobomb',
+    download_url="https://github.com/TheSpeedX/TBomb/archive/pypi.zip",
+        keywords=['android', 'spam', 'sms', 'mail', 'tbomb',
+                  'call', 'bomb', 'termux', 'bomber', 'spammer'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    install_requires=requirements,
+    license='GPL',
+    entry_points={
+            'console_scripts': [
+                'tbomb = tbomb.bomber:main',
+            ],
+    },
+    python_requires='>=3.5'
+)
